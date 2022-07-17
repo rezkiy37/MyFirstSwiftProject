29bac976db5a9315cfbc9e3fd7d7fa46195f472b
Mykhailo Kravchenko <rezkiy37@gmail.com>
2022-07-17 23:04:56 +0200

    Initial Commit

---
+++ MyFirstSwiftProject/MyFirstSwiftProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstSwiftProjectApp.swift
+//  MyFirstSwiftProject
+//
+//  Created by Mykhailo Kravchenko on 17.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstSwiftProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
